cc6e294948c7490d0fb5e2fa779d049050218a8a
yook <yhj03069@gmail.com>
2023-02-06 14:20:00 +0900

    Initial Commit

---
+++ 20230206circleTracker/_0230206circleTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0230206circleTrackerApp.swift
+//  20230206circleTracker
+//
+//  Created by yook on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct _0230206circleTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
